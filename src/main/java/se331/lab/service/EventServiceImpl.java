15557ec44bd04a9e988b750ef6f66b3004c995ad
kaungkaung <kaunghtoo764@gmail.com>
2026-09-21 11:44:47 +0700

    finished step 6 lab9

--- src/main/java/se331/lab/service/EventServiceImpl.java
+++ src/main/java/se331/lab/service/EventServiceImpl.java
@@ -2,9 +2,14 @@ package se331.lab.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.dao.EventDao;
+import se331.lab.dao.OrganizerDao;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 
 import java.util.List;
 
@@ -12,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     final EventDao eventDao;
+    final OrganizerDao organizerDao;
 
     @Override
     public Integer getEventSize() {
@@ -29,7 +35,16 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+    @Transactional
     public Event save(Event event) {
+        if (event.getOrganizer() == null || event.getOrganizer().getId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The organizer id is required");
+        }
+        Organizer organizer = organizerDao.findById(event.getOrganizer().getId())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Organizer not found"));
+        event.setOrganizer(organizer);
+        organizer.getOwnEvents().add(event);
         return eventDao.save(event);
     }
 }
--- src/main/java/se331/lab/util/LabMapper.java
+++ src/main/java/se331/lab/util/LabMapper.java
@@ -2,8 +2,7 @@ package se331.lab.util;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import se331.lab.entity.Event;
-import se331.lab.entity.EventDTO;
+import se331.lab.entity.*;
 
 import java.util.List;
 
@@ -12,4 +11,6 @@ public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
     List<EventDTO> getEventDto(List<Event> events);
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
 }
