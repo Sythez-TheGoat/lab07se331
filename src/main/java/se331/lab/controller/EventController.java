21a5aac0d0d9b5ec08dd7b6ff81b137a23f0f1f2
kaungkaung <kaunghtoo764@gmail.com>
2026-09-21 10:48:00 +0700

    finished step 3 lab9

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -1,6 +1,6 @@
 package se331.lab.controller;
 
-import lombok.RequiredArgsConstructor;
+import  lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab.entity.Event;
 import se331.lab.service.EventService;
+import se331.lab.util.LabMapper;
 
 import java.util.List;
 
@@ -24,18 +25,16 @@ public class EventController {
         Page<Event> pageOutput = eventService.getEvents(perPage, page);
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
-        try {
-            return ResponseEntity.ok().headers(responseHeaders).body(pageOutput.getContent());
-        } catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.ok().headers(responseHeaders).body(pageOutput.getContent());
-        }
+        return new ResponseEntity<>(
+                LabMapper.INSTANCE.getEventDto(pageOutput.getContent()),
+                responseHeaders, HttpStatus.OK);
     }
 
     @GetMapping("events/{id}")
     public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
         Event output = eventService.getEvent(id);
         if (output != null) {
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
@@ -44,6 +43,6 @@ public class EventController {
     @PostMapping("/events")
     public ResponseEntity<?> addEvent(@RequestBody Event event) {
         Event output = eventService.save(event);
-        return ResponseEntity.ok(output);
+        return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
     }
 }
